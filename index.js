4a8a351dd9ac6d8878d4bc348d706ec23e5acea6
David Sundelius <david.sundelius@gmail.com>
2018-07-09 00:39:49 +0200

    Updated the hard coded response to figure out how the response api works

--- index.js
+++ index.js
@@ -7,7 +7,6 @@ const APP_ID = 'amzn1.ask.skill.c888b9c1-772f-4f13-8f2c-487e969dee6a';
 
 const handlers = {
     'NewSession': function () {
-        this.attributes.speechOutput = this.t('WELCOME_MESSAGE', '60', 'Masthugget', '12:45', 'Vagnhallen Gårda');
         this.emit('SessionEndedRequest');
     },
     'AMAZON.HelpIntent': function () {
@@ -23,7 +22,7 @@ const handlers = {
         this.emit('SessionEndedRequest');
     },
     'SessionEndedRequest': function () {
-        this.emit(':tell', this.t('STOP_MESSAGE'));
+        this.emit(':tell', this.t('MESSAGE', '60', 'Masthugget', '12:45', 'Vagnhallen Gårda'));
     },
 };
 
@@ -31,8 +30,7 @@ const languageStrings = {
     'en-US': {
         translation: {
             RECIPES: recipes.RECIPE_EN_US,
-            WELCOME_MESSAGE: "Your next commute with number %s headed for %s will leave at %s from %s",
-            STOP_MESSAGE: 'Goodbye!',
+            MESSAGE: "Your next commute with number %s headed for %s will leave at %s from %s"
         }
     }
 };
